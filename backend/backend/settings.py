f8279848523a9626a396038709e5688bd20a4a15
shahriar <shahriarksaurov@gmail.com>
2023-12-21 22:45:40 +0200

    Job backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,7 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'corsheaders',
+    #'storages'
     'django.contrib.gis',
+    'django_filters',
+    'job.apps.JobConfig'
 ]
 
 MIDDLEWARE = [
