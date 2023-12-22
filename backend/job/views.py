1de8f1366397b9085e57d505b2847292bdf56a48
shahriar <shahriarksaurov@gmail.com>
2023-12-22 10:05:53 +0200

    search, filter and pagination

--- backend/job/views.py
+++ backend/job/views.py
@@ -3,20 +3,33 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.db.models import Avg, Min, Max, Count
-
+from .filters import JobsFilter
 from .serializers import JobSerializer
 from .models import Job
-
+from rest_framework.pagination import PageNumberPagination
 from django.shortcuts import get_object_or_404
 # Create your views here.
 
 @api_view(['GET'])
 def getAllJobs(request):
 
-    jobs = Job.objects.all()
+    #jobs = Job.objects.all()
+    filterset = JobsFilter(request.GET, queryset=Job.objects.all().order_by('id'))
+    count = filterset.qs.count()
 
-    serializer = JobSerializer(jobs, many=True)
-    return Response(serializer.data)
+    # Pagination
+    resPerPage = 3
+    paginator = PageNumberPagination()
+    paginator.page_size = resPerPage
+
+    queryset = paginator.paginate_queryset(filterset.qs, request)
+    
+    serializer = JobSerializer(queryset, many=True)
+    return Response({
+        "count": count,
+        "resPerPage": resPerPage,
+        'jobs': serializer.data
+        })
 
 @api_view(['GET'])
 def getJob(request, pk):
